0cb2f69f05b173cb8775b7a9466a503f003e5c75
unknown <railsonp560@gmail.com>
2023-11-10 19:14:13 -0300

    update readme

--- djangoTenants/TenantClient/models.py
+++ djangoTenants/TenantClient/models.py
@@ -9,9 +9,7 @@ from django_tenants.models import TenantMixin, DomainMixin
 
 class Client(TenantMixin):
     nome = models.CharField(max_length=100)
-    trial = models.BooleanField()
-    inicio_licenca = models.DateField(auto_now_add=True)
-    inicio_fim = models.DateField(null=True)
+    
     
     auto_create_schema = True
 
